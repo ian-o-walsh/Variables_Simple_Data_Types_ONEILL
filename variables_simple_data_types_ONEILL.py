4a9282f2009da5287bb2a53d86a9177fc1b4d9c8
Ian O'Neill <ioneill@mail.walshcollege.edu>
2025-11-05 22:33:41 -0500

    Started the assignment

---
+++ variables_simple_data_types_ONEILL.py
@@ -0,0 +1,11 @@
+# Ian O'Neill
+# IT 410
+# Week 6 - Variables and Simple Data Types Assignment
+# Professor Charnesky
+
+# Store first and last name as variables
+first_name = "ian"
+last_name = "ONEILL"
+
+# Print greeting with name conversions
+print("Hello, " + first_name.upper() + " " + last_name.lower())
