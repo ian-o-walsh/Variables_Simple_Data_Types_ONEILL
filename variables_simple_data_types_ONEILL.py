97d75eb3ed83927636a82424de55ff2dc6f10182
Ian O'Neill <ioneill@mail.walshcollege.edu>
2025-11-05 22:37:18 -0500

    added more progress

--- variables_simple_data_types_ONEILL.py
+++ variables_simple_data_types_ONEILL.py
@@ -9,3 +9,25 @@ last_name = "ONEILL"
 
 # Print greeting with name conversions
 print("Hello, " + first_name.upper() + " " + last_name.lower())
+
+# Print two newlines
+print("\n\n")
+
+# Print the quote with quotes at beginning and end
+print('"Start by doing what\'s necessary; then do what\'s possible; and suddenly you are doing the impossible - Francis of Assisi"')
+
+# Two decimal numbers
+num1 = 12.5
+num2 = 3.5
+
+# Performing addition, subtraction, multiplication, and division
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Print results for each operation
+print(str(num1) + " plus " + str(num2) + " equals " + str(addition))
+print(str(num1) + " minus " + str(num2) + " equals " + str(subtraction))
+print(str(num1) + " times " + str(num2) + " equals " + str(multiplication))
+print(str(num1) + " divided by " + str(num2) + " equals " + str(division))
